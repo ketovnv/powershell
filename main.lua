925c270581dc5d8d6b5cd78fe3971fb6fb2583e1
ketov <ketovnv@gmail.com>
2025-07-26 09:49:35 +0300

    lua

--- main.lua
+++ main.lua
@@ -1,4 +1,15 @@
----
---- Created by ketov.
---- DateTime: 26.07.2025 8:04
----
+-- Load the debugger module
+local dbg = require('emmy_core')
+
+dbg.tcpListen('localhost', 9966)
+-- Start the TCP debug server
+
+package.cpath = package.cpath .. ';C:/Users/ketov/AppData/Roaming/JetBrains/WebStorm2025.1/plugins/IntelliJ-EmmyLua2/debugger/emmy/windows/x64/?.dll'
+-- Wait for IDE connection
+dbg.waitIDE()
+
+-- Set a strong breakpoint here
+dbg.breakHere()
+
+-- Your Lua code
+print("Hello, EmmyLua Debugger!")
